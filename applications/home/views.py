05a9f5da9fcfcf0aaea78aa55397b7d849f946c9
unknown <roberthbardales@hotmail.com>
2026-05-05 23:07:21 -0500

    mejora visual

--- applications/home/views.py
+++ applications/home/views.py
@@ -6,3 +6,6 @@ class HomeView(ListView):
     model = Product
     template_name = 'home.html'
     context_object_name = 'products'
+
+    def get_queryset(self):
+        return Product.objects.filter(is_available=True).order_by('-created_date')[:8]
